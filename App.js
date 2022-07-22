562dc6b8fa53eb6b32eed1d818ac06af9ee53ce7
Subrata34 <subratamodak3491@gmail.com>
2022-07-23 00:29:48 +0600

    files

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {colors} from './src/theme/colors';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
+  const [fontsLoaded]=useFonts({
+    'Kanit-Bold':require('./assets/fonts/Kanit-Bold.ttf'),
+    'Poppin-BoldItalic':require('./assets/fonts/Poppin-BoldItalic.ttf'),
+    'JosefinSans-VariableFont_wght':require('./assets/fonts/JosefinSans-VariableFont_wght.ttf')
+
+  });
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
   return (
     <View style={styles.container}>
-      <Text style={{color:'white'}}>Open up App.js  your app! react native </Text>
+      <Text style={{color:'white'}}>Open up App.js   app! react native </Text>
       <StatusBar style="auto" />
     </View>
   );
