1454541cc2b76fb8cd7242722d0afe79f0dafb71
Subrata34 <subratamodak3491@gmail.com>
2022-07-22 17:01:09 +0600

    files

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:colors.blue,
+    backgroundColor:colors.grey,
     alignItems: 'center',
     justifyContent: 'center',
   },
